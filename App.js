fdce566c47c3279cfa3206d06379cc7ddc2af70f
komal682 <63336449+komal682@users.noreply.github.com>
2022-12-03 17:16:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PathFindingVisualizer from './PathFindingVisualizer/PathFindingVisualizer';
+
+function App() {
+  return (
+    <div className="App">
+      <PathFindingVisualizer></PathFindingVisualizer>
+    </div>
+  );
+}
+
+export default App;
